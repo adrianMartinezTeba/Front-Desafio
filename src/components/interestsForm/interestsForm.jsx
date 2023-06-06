ac68b2b65c0564f8a27205def679965bfb7426b3
adrianMartinezTeba <adrymate@gmail.com>
2023-06-06 04:01:04 +0200

    añadiendo forma home

--- src/components/interestsForm/interestsForm.jsx
+++ src/components/interestsForm/interestsForm.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const interestsForm = () => {
+const InterestsForm = () => {
+  const interests = ['caca','pis','hola','iewbska'];
+  const [selectedInterests, setSelectedInterests] = useState([]);
+//onsubmit un put de user para que se actualice su array de intereses 
+  const handleInterestChange = (event) => {
+    const interest = event.target.value;
+    if (event.target.checked) {
+      setSelectedInterests((prevSelectedInterests) => [...prevSelectedInterests, interest]);
+    } else {
+      setSelectedInterests((prevSelectedInterests) =>
+        prevSelectedInterests.filter((item) => item !== interest)
+      );
+    }
+  };
+  const onSubmit = () =>{
+    
+  }
   return (
     <div>
-     <h1>interestsForm</h1> 
-    
+      <h1>Interests Form</h1>
+      <form>
+        {interests.map((interest) => (
+          <div key={interest}>
+            <label>
+              <input
+                type="checkbox"
+                value={interest}
+                checked={selectedInterests.includes(interest)}
+                onChange={handleInterestChange}
+              />
+              {interest}
+            </label>
+          </div>
+        ))}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default interestsForm
+export default InterestsForm;
