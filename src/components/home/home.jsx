4ebad1284fbf0220a03eb90bb8290353efbe3a75
adrianMartinezTeba <adrymate@gmail.com>
2023-06-12 02:44:14 +0200

    ahora guarda bien en home

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -16,77 +16,80 @@ const Home = () => {
 
   useEffect(() => {
     getAllNews();
-    getAllEvents()
-   getUserLogged()
+    getAllEvents();
+   getUserLogged();
+   setTimeout(() => {
+     setNoticias(news);
+     setUserIMG(user)
+     setAllEvents(events)
+    
+   }, 500);
   },[]);
 
   useEffect(() => {
-    setNoticias(news);
-    setAllEvents(events)
-    setUserIMG(user)
     console.log(news);
     console.log(events);
-    console.log(user);
+    console.log(userIMG);
     
-  }, [news,events,user]);
-  const imgPerfil = <img src={userIMG.imageUrl} width={'200px'} />
-  const divAllNoticias = noticias.map((noticia) => (
-    <div>
-      <div key={noticia.id}>
-        <div>
-          {noticia.tags.map((tag) => (
-            <div key={tag}>
-              <span>{tag}</span>
-            </div>
-          ))}
-        </div>
-        <img src={noticia.imgUrl} alt="Imagen" />
-        <p>{noticia.date}</p>
-        <p>{noticia.body}</p>
-      </div>
-    </div>
-  ));
-  const divAllEvents = allEvents.map((eventElement)=>(
-    <div key={eventElement.id}>
-      <img src={eventElement.imageUrl} />
-      <div>
-        {eventElement.name}
-      </div>
-      <div>
-        <span>{eventElement.startDate}</span>
-      </div>
-      <div>
-        {eventElement.tags.map((tag)=>(
-          <button>{tag}</button>
-        ))}
-      </div>
+  }, [news,user]);
+  // const imgPerfil = <img src={userIMG.imageUrl} width={'200px'} />
+  // const divAllNoticias = noticias.map((noticia) => (
+  //   <div>
+  //     <div key={noticia.id}>
+  //       <div>
+  //         {noticia.tags.map((tag) => (
+  //           <div key={tag}>
+  //             <span>{tag}</span>
+  //           </div>
+  //         ))}
+  //       </div>
+  //       <img src={noticia.imgUrl} alt="Imagen" />
+  //       <p>{noticia.date}</p>
+  //       <p>{noticia.body}</p>
+  //     </div>
+  //   </div>
+  // ));
+  // const divAllEvents = allEvents.map((eventElement)=>(
+  //   <div key={eventElement.id}>
+  //     <img src={eventElement.imageUrl} />
+  //     <div>
+  //       {eventElement.name}
+  //     </div>
+  //     <div>
+  //       <span>{eventElement.startDate}</span>
+  //     </div>
+  //     <div>
+  //       {eventElement.tags.map((tag)=>(
+  //         <button>{tag}</button>
+  //       ))}
+  //     </div>
 
-    </div>
-  ))
+  //   </div>
+  // ))
 
 
   return (
     <div className="home-container">
       <div className="noticias-container">
         <h3>Noticias</h3>
-        <Link to="/profile">
+        {/* <Link to="/profile">
           {imgPerfil}
-        </Link>
-        {noticias.length > 0 ? (
+        </Link> */}
+        {/* {noticias.length > 0 ? (
           divAllNoticias
         ) : (
           <p>No hay noticias disponibles.</p>
-        )}
+        )} */}
       </div>
       <div className="recientes-container">
         <p>Recientes</p>
         <button>Ver todo</button>
         <hr />
-        {noticias.length > 0 ? (
+        {/* {noticias.length > 0 ? (
           divAllNoticias
         ) : (
           <p>No hay noticias disponibles.</p>
-        )}
+        )} */}
       </div>
       <Menu />
     </div>
