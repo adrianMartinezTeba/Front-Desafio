78bb1c67939ab4a8d375f56afac05b9b5293ef8d
valenti94br <valenti94br@gmail.com>
2023-06-13 18:07:26 +0200

    event con scss medio bien

--- src/components/NewIndividual/NewIndividual.jsx
+++ src/components/NewIndividual/NewIndividual.jsx
@@ -52,4 +52,3 @@ const NewIndividual = () => {
 };
 
 export default NewIndividual;
-
--- src/components/eventoIndividual/eventoIndividual.jsx
+++ src/components/eventoIndividual/eventoIndividual.jsx
@@ -1,37 +1,52 @@
-// import React, { useEffect, useContext } from "react";
+
+// import React, { useEffect, useContext, useState } from "react";
 // import { useParams } from "react-router-dom";
 // import { EventContext } from "../../context/EventContext/EventState";
-// import Menu from "../Menu/Menu"; // Importar el componente Menu
+// import Menu from "../Menu/Menu";
+// import "./EventoIndividual.scss";
 
 // const EventoIndividual = () => {
 //   const { id } = useParams();
 //   const { event, getEventById } = useContext(EventContext);
+//   const [isBodyExpanded, setIsBodyExpanded] = useState(false);
 
 //   useEffect(() => {
 //     getEventById(id);
 //   }, []);
-//   useEffect(()=>{
-    
-//   },[event])
 
 //   return (
-//     <div>
+//     <div className="container">
 //       <Menu />
-//       <h1>EventoIndividual</h1>
-//       {event ? (
-//         <>
-//           <h2>{event.name}</h2>
-//           <p>{event.description}</p>
-//           <p>Fecha: {event.date}</p>
-//           <p>Hora de inicio: {event.startDate}</p>
-//           <p>Hora de finalización: {event.endDate}</p>
-//           <p>Localización: {event.localization}</p>
-//           <p>Interés: {event.interest}</p>
-//           {/* Aquí puedes mostrar otros detalles del evento */}
-//         </>
-//       ) : (
-//         <p>Cargando evento...</p>
-//       )}
+//       <div className={`evento ${isBodyExpanded ? "expanded" : ""}`}>
+//         {event ? (
+//           <>
+//             <div className="header">
+//               <img className="imagen-fondo" src={event.imageUrl} alt={event.title} />
+//               <div className="overlay">
+//                 <h2 className="title">{event.name}</h2>
+//                 <p className="date">Fecha: {event.date}</p>
+//               </div>
+//             </div>
+//             <div className="body">
+//               <p className="description">{event.description}</p>
+//               <div className="time-info">
+//                 <p className="start-time">Hora de inicio: {event.startDate}</p>
+//                 <p className="end-time">Hora de finalización: {event.endDate}</p>
+//               </div>
+//               <p className="location">Localización: {event.localization}</p>
+//               <p className="interest">Interés: {event.interest}</p>
+//             </div>
+//             <div
+//               className="expand-button"
+//               onClick={() => setIsBodyExpanded(!isBodyExpanded)}
+//             >
+//               {isBodyExpanded ? "Minimizar" : "Expandir"}
+//             </div>
+//           </>
+//         ) : (
+//           <p>Cargando evento...</p>
+//         )}
+//       </div>
 //     </div>
 //   );
 // };
@@ -42,53 +57,46 @@ import React, { useEffect, useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { EventContext } from "../../context/EventContext/EventState";
 import Menu from "../Menu/Menu";
-import "./EventoIndividual.scss";
+import "./eventoIndividual.scss";
+import expandir from "../../assets/icons/barra_deslizamiento.png";
 
 const EventoIndividual = () => {
   const { id } = useParams();
   const { event, getEventById } = useContext(EventContext);
-  const [isBodyExpanded, setIsBodyExpanded] = useState(false);
+  const [isTextExpanded, setIsTextExpanded] = useState(false);
 
   useEffect(() => {
     getEventById(id);
-  }, []);
+  }, [id, getEventById]);
 
   return (
     <div className="container">
       <Menu />
-      <div className={`evento ${isBodyExpanded ? "expanded" : ""}`}>
-        {event ? (
-          <>
-            <div className="header">
-              <img className="imagen-fondo" src={event.imageUrl} alt={event.title} />
-              <div className="overlay">
-                <h2 className="title">{event.name}</h2>
-                <p className="date">Fecha: {event.date}</p>
-              </div>
+      {event ? (
+        <>
+          <div className="fondo">
+            <img className="imagen-fondo" src={event.imageUrl} alt={event.title} />
+            <div className="overlay">
+              <h2>{event.title}</h2>
             </div>
-            <div className="body">
-              <p className="description">{event.description}</p>
-              <div className="time-info">
-                <p className="start-time">Hora de inicio: {event.startDate}</p>
-                <p className="end-time">Hora de finalización: {event.endDate}</p>
-              </div>
-              <p className="location">Localización: {event.localization}</p>
-              <p className="interest">Interés: {event.interest}</p>
+          </div>
+          <div className={`texto ${isTextExpanded ? "expanded" : ""}`}>
+            <div className="expand-button" onClick={() => setIsTextExpanded(!isTextExpanded)}>
+              {isTextExpanded ? (
+                <img src={expandir} alt="Minimizar" />
+              ) : (
+                <img src={expandir} alt="Expandir" />
+              )}
             </div>
-            <div
-              className="expand-button"
-              onClick={() => setIsBodyExpanded(!isBodyExpanded)}
-            >
-              {isBodyExpanded ? "Minimizar" : "Expandir"}
-            </div>
-          </>
-        ) : (
-          <p>Cargando evento...</p>
-        )}
-      </div>
+            <p>Fecha de publicación: {new Date(event.date).toLocaleDateString()}</p>
+            <p>{event.body}</p>
+          </div>
+        </>
+      ) : (
+        <p>Cargando evento...</p>
+      )}
     </div>
   );
 };
 
 export default EventoIndividual;
-
