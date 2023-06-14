34c03cd51cd52bdab777a91f0245ca4d09662b37
Goner10 <zalito_96@hotmail.com>
2023-06-14 21:18:26 +0200

    dejando guay el cuestionario

--- src/components/interestsForm/interestsForm.jsx
+++ src/components/interestsForm/interestsForm.jsx
@@ -7,28 +7,28 @@ import { TagContext } from '../../context/TagsContext/TagContext';
 
 
 const InterestsForm = () => {
-  const { user, getUserLogged,update } = useContext(UserContext);
-  const {tags,getTags}=useContext(TagContext)
+  const { user, getUserLogged, update } = useContext(UserContext);
+  const { tags, getTags } = useContext(TagContext)
   const [currentView, setCurrentView] = useState(1);
   const [previousView, setPreviousView] = useState(null);
-  
-  
- 
+
+
+
 
   useEffect(() => {
-getTags(),
-getUserLogged()
+    getTags(),
+      getUserLogged()
 
   }, []);
   useEffect(() => {
- console.log(user);
- console.log(tags);
-  }, [user,tags]);
-  
+    console.log(user);
+    console.log(tags);
+  }, [user, tags]);
+
   const handleContinue = () => {
     setPreviousView(currentView);
     setCurrentView(currentView + 1);
-  
+
   };
 
   const handleBack = () => {
@@ -40,155 +40,156 @@ getUserLogged()
       case 1:
         return (
           <>
+
             <div className="profile-container">
               <div className="info-user">
                 {currentView === 1 && (
                   <div className="user-details">
+
                     <label htmlFor="profileImage" className="file-input-label">
                       <div className="file-input-circle"></div>
                       <input type="file" id="profileImage" className="file-input" />
+                      📷
                     </label>
-                    <h2>{user.name}</h2>
-                    <h2>{user.email}</h2>
+
+                    <div className='user-info'>
+                      <h2>{user.name}</h2>
+                      <h2>{user.email}</h2>
+                    </div>
                   </div>
                 )}
-      
+
                 <label htmlFor="name">Nombre y apellido</label>
                 <input type="text" id="name" value={user.name} />
-      
+
                 <label htmlFor="Grado">Grado</label>
                 <input type="text" id="Grado" value={user.course} />
-      
+
                 <label htmlFor="fecha">Fecha de nacimiento</label>
                 <input type="text" id="fecha" value={user.birth_date} />
-      
+
                 <label htmlFor="pais">Pais de procedencia</label>
                 <input type="text" id="pais" value={user.country} />
-      
+
                 <label htmlFor="situacion">Situación laboral</label>
                 <input type="text" id="situacion" value={user.situationLaboral} />
-      
-                <div className="languages-container">
-                  <h3 className="languages">Idiomas</h3>
-                </div>
-      
+
+
+
                 <button onClick={handleContinue} className="button2">Continuar</button>
               </div>
             </div>
           </>
         );
-      
+
       case 2:
         return (
-          <>
-          
-        
-          <div className="info-user">
-             <div className="user-details">
-             <div className="profile-image-container">
-             <img src={user.imageUrl} alt="" width={'59px'}/>
-             </div>
-             <div className='user-info'>
-            <h2>{user.name}</h2>
-            <h2>{user.email}</h2>
+          <div className="profile-container">
+            <div className="info-user">
+              <div className="user-details">
+                <div className="profile-image-container">
+                  <img src={user.imageUrl} alt="" width={'59px'} />
+                </div>
+                <div className='user-info'>
+                  <h2>{user.name}</h2>
+                  <h2>{user.email}</h2>
+                </div>
+              </div>
+
+              <h3 className='view2'>Notificaciones</h3>
+              <div className="label-input-container">
+                <label htmlFor="eventos">Nuevos eventos</label>
+                <select id="eventos" className="select-input">
+                  <option value="opcion1">Contactos</option>
+                  <option value="opcion2">EDEM</option>
+                  <option value="opcion3">Lanzadera</option>
+                </select>
+              </div>
+
+              <div className="label-input-container">
+                <label htmlFor="mensajes">Mensajes</label>
+                <select id="mensajes" className="select-input">
+                  <option value="seguidos">Seguidos</option>
+                  <option value="todos">Todos</option>
+                </select>
+              </div>
+
+              <div className="label-input-container">
+                <label htmlFor="noticias">Noticias</label>
+                <select id="noticias" className="select-input">
+                  <option value="edem">EDEM</option>
+                  <option value="contactos">Contactos</option>
+                  <option value="todos">Todos</option>
+                </select>
+              </div>
+
+
+
+              <h3 className='view2'>Privacidad</h3>
+              <div className="label-input-container">
+                <label htmlFor="foto">Foto de perfil</label>
+                <select id="foto" className="select-input">
+                  <option value="contactos">Contactos</option>
+                  <option value="todos">Todos</option>
+                </select>
+              </div>
+              <div className="label-input-container">
+                <label htmlFor="correo">Correo</label>
+                <select id="correo" className="select-input">
+                  <option value="contactos">Contactos</option>
+                  <option value="todos">Todos</option>
+                </select>
+              </div>
+
+              <div className="label-input-container">
+                <label htmlFor="correo">Estudios</label>
+                <select id="estudios" className="select-input">
+                  <option value="contactos">Contactos</option>
+                  <option value="todos">Todos</option>
+                </select>
+              </div>
+
+              <div className="button-container">
+                <button className='button2' onClick={handleBack}>Atrás</button>
+                <button className='button2' onClick={handleContinue}>Continuar</button>
+              </div>
             </div>
-            </div>
-            
-<h3 className='view2'>Notificaciones</h3>
-<div className="label-input-container">
-<label htmlFor="eventos">Nuevos eventos</label>
-        <select id="eventos" className="select-input">
-          <option value="opcion1">Contactos</option>
-          <option value="opcion2">EDEM</option>
-          <option value="opcion3">Lanzadera</option>
-        </select>
-</div>
 
-<div className="label-input-container">
-        <label htmlFor="mensajes">Mensajes</label>
-        <select id="mensajes" className="select-input">
-          <option value="seguidos">Seguidos</option>
-          <option value="todos">Todos</option>
-        </select>
-      </div>
-
-      <div className="label-input-container">
-        <label htmlFor="noticias">Noticias</label>
-        <select id="noticias" className="select-input">
-          <option value="edem">EDEM</option>
-          <option value="contactos">Contactos</option>
-          <option value="todos">Todos</option>
-        </select>
-      </div>
-      
-
-
-<h3 className='view2'>Privacidad</h3>
-<div className="label-input-container">
-        <label htmlFor="foto">Foto de perfil</label>
-        <select id="foto" className="select-input">
-          <option value="contactos">Contactos</option>
-          <option value="todos">Todos</option>
-        </select>
-      </div>
-      <div className="label-input-container">
-        <label htmlFor="correo">Correo</label>
-        <select id="correo" className="select-input">
-          <option value="contactos">Contactos</option>
-          <option value="todos">Todos</option>
-        </select>
-      </div>
-   
-      <div className="label-input-container">
-        <label htmlFor="correo">Estudios</label>
-        <select id="estudios" className="select-input">
-          <option value="contactos">Contactos</option>
-          <option value="todos">Todos</option>
-        </select>
-      </div>
-
-
-
-
-            <div className="button-container">
-            <button  className='button2' onClick={handleBack}>Atrás</button>
-            <button className='button2'  onClick={handleContinue}>Continuar</button>
-            </div>
           </div>
-          
-          </>
         );
       case 3:
         return (
-          <div className="info-user">
-             <div className="user-details">
-             <div className="profile-image-container">
-             <img src={user.imageUrl} alt="" width={'59px'}/>
-             </div>
-             <div className="user-info">
-            <h2>{user.name}</h2>
-            <h2>{user.email}</h2>
-            </div>
-            </div>
+          <div className="profile-container">
+            <div className="info-user">
+              <div className="user-details">
+                <div className="profile-image-container">
+                  <img src={user.imageUrl} alt="" width={'59px'} />
+                </div>
+                <div className="user-info">
+                  <h2>{user.name}</h2>
+                  <h2>{user.email}</h2>
+                </div>
+              </div>
 
-            <div>
-            <h3 className='view2'>Conocimientos</h3>
-            
-            </div>
-            <div>
-            <h3 className='view2'>Quiero aprender</h3>
-            </div>
-            <div>
-            <h3 className='view2'>Ocio</h3>
-            </div>
-            <div>
-            <h3 className='view2'>Intereses</h3>
-            </div>
+              <div>
+                <h3 className='view2'>Conocimientos</h3>
+
+              </div>
+              <div>
+                <h3 className='view2'>Quiero aprender</h3>
+              </div>
+              <div>
+                <h3 className='view2'>Ocio</h3>
+              </div>
+              <div>
+                <h3 className='view2'>Intereses</h3>
+              </div>
 
 
-            <div className="button-container">
-            <button onClick={handleBack} className='button2'>Atrás</button>
-            <Link to="/home" className='link-home'>Terminar</Link>
+              <div className="button-container">
+                <button onClick={handleBack} className='button2'>Atrás</button>
+                <Link to="/home" className='link-home'>Terminar</Link>
+              </div>
             </div>
           </div>
         );
@@ -198,11 +199,11 @@ getUserLogged()
   };
 
   return (
-    <>
-      <div className="profile-container">
-        {renderView()}
-      </div>
-    </>
+
+    <div className="principal-container">
+      {renderView()}
+    </div>
+
   );
 };
 
