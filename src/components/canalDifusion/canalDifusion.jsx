2b0993cfd3ff33338726c734f8122299876c0ba3
valenti94br <valenti94br@gmail.com>
2023-06-15 14:18:48 +0200

    canalDifusion tocado

--- src/components/canalDifusion/canalDifusion.jsx
+++ src/components/canalDifusion/canalDifusion.jsx
@@ -1,11 +1,15 @@
 
 import React, { useEffect, useContext, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import { CanalDifusionContext } from "../../context/CanalDifusionContext/CanalDifusionState";
 import { EventContext } from "../../context/EventContext/EventState";
 import Menu from "../Menu/Menu";
+import mathplus from "../../assets/icons/math-plus.png";
+import './CanalDifusion.scss';
+
 
 const CanalDifusion = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const { canalDifusion, getCanalDifusionById } = useContext(CanalDifusionContext);
   const { events, getAllEvents } = useContext(EventContext);
@@ -33,26 +37,27 @@ const CanalDifusion = () => {
   }, [canalDifusion, events]);
 
   return (
-    <div>
+    <div className="prim">
       <Menu />
+      <button className="botoncete" onClick={() => navigate("/createEvent")}><img className="fotito" src={mathplus} alt="math-plush"/></button>
       <h2>Canal</h2>
       {canalDifusion ? (
         <>
-          <h2>{canalDifusion.name}</h2>
+          <h2 className="hdos">{canalDifusion.name}</h2>
           {/* <p>{canalDifusion.description}</p> */}
-          <h3>Eventos:</h3>
-          <div style={{ maxHeight: "300px", overflowY: "auto" }}>
+          <h3 className="htres">Eventos:</h3>
+          <div style={{ maxHeight: "300px", overflowY: "auto" }} className="prim">
             {filteredEvents.map((event) => (
-              <div key={event._id}>
+              <div key={event._id} className="prim">
                 <Link to={`/event/${event._id}`}>
-                  <h4>{event.name}</h4>
+                  <h4 className="hcuatro">{event.name}</h4>
                 </Link>
               </div>
             ))}
           </div>
         </>
       ) : (
-        <p>Cargando canal de difusión...</p>
+        <p className="pe">Cargando canal de difusión...</p>
       )}
     </div>
   );
